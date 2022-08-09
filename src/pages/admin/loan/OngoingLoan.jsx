d4ab518da79c89a3e5737b3932c71ee79ac045af
Itodo Simon <itodosimonitodo1@gmail.com>
2022-08-09 21:58:09 +0100

    errored

--- src/pages/admin/loan/OngoingLoan.jsx
+++ src/pages/admin/loan/OngoingLoan.jsx
@@ -1,13 +1,13 @@
-import {Fragment,useEffect,useContext,useState} from 'react'
+import { Fragment, useEffect, useContext, useState } from 'react'
 import { Link } from "react-router-dom";
 // import "./Dashboard.css"
 import { Formik, Form, Field } from "formik";
-import {object as yupObject,string as yupString,number as yupNumber} from "yup";
+import { object as yupObject, string as yupString, number as yupNumber } from "yup";
 import { toast } from "react-toastify";
 import { api } from '../../../services';
 import { css } from "@emotion/react";
-import {BounceLoader} from "react-spinners";
-import {Context} from "../../../context/Context";
+import { BounceLoader } from "react-spinners";
+import { Context } from "../../../context/Context";
 import PageTitle from "../../../components/PageTitle"
 import Widget from "../../../components/Widget/Widget";
 import useStyles from './styles';
@@ -20,6 +20,7 @@ import {
   Grid,
 } from "@material-ui/core";
 import ActionButton from './ActionButton';
+import Reciept from '../reciept';
 
 
 // CONTEXT
@@ -32,54 +33,54 @@ const override = css`
 
 
 function OngoingLoan() {
-    const classes = useStyles();
-    const [isLoading, setIsLoading] = useState(false);
-    let [loading, setLoading] = useState(true);
-    const [loader, setLoader] = useState(false);
-    let [color, setColor] = useState("#ADD8E6");
-    const {user} = useContext(Context)
-    const [data,setData] = useState([]);
+  const classes = useStyles();
+  const [isLoading, setIsLoading] = useState(false);
+  let [loading, setLoading] = useState(true);
+  const [loader, setLoader] = useState(false);
+  let [color, setColor] = useState("#ADD8E6");
+  const { user } = useContext(Context)
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        try {
-            setIsLoading(true)
+  useEffect(() => {
+    try {
+      setIsLoading(true)
 
-            const allCustomer = async() => {
-              const res = await api.service().fetch("/dashboard/loan/?status=PENDING",true);
-              console.log(res.data)
-              if(api.isSuccessful(res)){
-                setData(res.data.results)
-              }
-            setIsLoading(false);
-        
-            }
-    
-            allCustomer();
-        } catch (error) {
-            console.log(error)
+      const allCustomer = async () => {
+        const res = await api.service().fetch("/dashboard/loan/?status=PENDING", true);
+        console.log(res.data)
+        if (api.isSuccessful(res)) {
+          setData(res.data.results)
         }
-      },[])
+        setIsLoading(false);
 
-      const approve_loan = async(values) => {
-            try {
-                setLoader(true);
-                console.log(values)
-        
-                const response = await api
-                    .service()
-                    .push("/dashboard/loan/action/",values,true)
-        
-                if(api.isSuccessful(response)){
-                setTimeout( () => {
-                    toast.success("Successfully approved loan!");
-                    // navigate("/admin/dashboard/add_borrower",{replace: true});
-                },0);
-                }
-                setLoader(false);
-            } catch (error) {
-                console.log(error)
-            }
+      }
+
+      allCustomer();
+    } catch (error) {
+      console.log(error)
     }
+  }, [])
+
+  const approve_loan = async (values) => {
+    try {
+      setLoader(true);
+      console.log(values)
+
+      const response = await api
+        .service()
+        .push("/dashboard/loan/action/", values, true)
+
+      if (api.isSuccessful(response)) {
+        setTimeout(() => {
+          toast.success("Successfully approved loan!");
+          // navigate("/admin/dashboard/add_borrower",{replace: true});
+        }, 0);
+      }
+      setLoader(false);
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   return (
@@ -135,7 +136,7 @@ function OngoingLoan() {
               </Grid>
             )
         }
-
+        <Reciept />
       </Grid>
     </Fragment>
   )
